8a25d52bab957fc99c567a1448f652f134837f77
Arunava Chakraborty <arunavaskar@outlook.com>
2019-11-04 00:28:19 +0530

    Update Challenge11.js

--- Challenge11.js
+++ Challenge11.js
@@ -1,5 +1,29 @@
+// Given a string, return a new string where the first and last chars have been exchanged.
+//
+//
+// front_back('code') → 'eodc'
+// front_back('a') → 'a'
+// front_back('ab') → 'ba'
+
+//python code
+// def front_back(str):
+//   if len(str) <= 1:
+//       return str
+//   # elif len(str) >= 3:
+//   return str[-1] + str[1:-1] + str[0]
+//   # elif len(str) == 2:
+//   #     return str[-1] + str[0]
+
+
+
 function front_back(str){
+  //covers if str is "" or "a"
   if (str.length <= 1){
-      return str}
-  return str.substr(str.length) + str.substr(1,str.length) + str.substr(0)
+      return str
+    }
+  else if (str.length == 2)
+  {
+    return str[str.length -1] + str[0]
+  }
+  return str[str.length -1] + str.slice(1,str.length - 1) +str[0]
 }
